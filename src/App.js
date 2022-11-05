9192d2c869859b11acb5e5297ac78727ebd72bfa
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-11-05 14:07:10 -0700

    Removed excess code and images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react"
 import './App.css';
 
 class App extends React.Component {
+	state = {
+		event1: '12/25/2022',
+		event2: ''
+	}
 
 	render() {
 		return (
 			<>
 				<h1>Hello World</h1>
-				<span>This is a page</span>
+				<span>This is a {this.state.event1} page</span>
 			</>
 		)
 	}
