662813788ba702ff12d992848dd964d56bc4ed51
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-11-23 09:30:08 -0700

    Updated DispCard to use State

--- src/DispCard.js
+++ src/DispCard.js
@@ -3,18 +3,41 @@ import './App.css'
 
 
 class DispCard extends React.Component {
-
+	state = {
+		birth1: '03/03/2022',
+		xmas: '12/25/2022',
+		nyd: '1/1/2023',
+		timeLeft1: ''
+	}
+	
+	calcDays = () => {
+		let year = new Date().getFullYear()
+		let difference = +new Date('12/25/${year}') - +new Date()
+		
+		if (difference > 0) {
+			this.state.timeLeft1 = {
+					days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+					hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+					minutes: Math.floor((difference / 1000 / 60) % 60),
+					seconds: Math.floor((difference / 1000) % 60)
+				  }
+			return this.state.timeLeft1	
+		}
+		
+		}
+	
 
 	render() {
 		return (
 			<>
 				<figure className="image-block">
-					<h1>44 days</h1>
+					<h1>40 days</h1>
 					<img src="https://hips.hearstapps.com/hmg-prod/images/interior-christmas-magic-glowing-tree-fireplace-royalty-free-image-1628537941.jpg" alt="" />
 					<figcaption>
 						<h3>
-							Until Christmas 2023
+							Until Christmas 2022
 						</h3>
+						<h5>{this.state.xmas}</h5>
 						<p>Will occur at our house after the guy in the big red suit visits. Presents. Brunch. Good times.</p>
 						<button>
 							Edit Info
@@ -23,12 +46,13 @@ class DispCard extends React.Component {
 				</figure>
 
 				<figure className="image-block">
-					<h1>51 days</h1>
-					<img src="https://d.newsweek.com/en/full/1960033/new-year-refers-december-31.jpg" alt="" />
+					<h1>47 days</h1>
+					<img src="https://townsquare.media/site/385/files/2017/06/RS8796_453917889.jpg?w=980&q=75" alt="" />
 					<figcaption>
 						<h3>
 							Until New Year 2023
 						</h3>
+						<h5>{this.state.nyd}</h5>
 						<p>Let's celebrate the ending of 2022 and the beginning of 2023. Dancing. Drinks. Good times.</p>
 						<button>
 							Edit Info
@@ -37,12 +61,13 @@ class DispCard extends React.Component {
 				</figure>
 
 				<figure className="image-block">
-					<h1>112 days</h1>
-					<img src="https://hips.hearstapps.com/hmg-prod/images/birthday-cake-decorated-with-colorful-sprinkles-and-royalty-free-image-1653509348.jpg" alt="" />
+					<h1>108 days</h1>
+					<img src="https://thumbs.dreamstime.com/b/happy-birthday-party-triangle-flags-confetti-background-vector-illustration-60000502.jpg" alt="" />
 					<figcaption>
 						<h3>
 							Until Will's Birthday
 						</h3>
+						<h5>{this.state.birth1}</h5>
 						<p>The big 4-0! Let's celebrate! Good times to be had!</p>
 						<button>
 							Edit Info
